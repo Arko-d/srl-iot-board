906d22bf57d597475b55a756f90b96fe9ac430dd
Arko-d <50272858+Arko-d@users.noreply.github.com>
2024-10-02 09:35:05 +0000

    Documented TB600B driver code

--- Drivers/Micropython/TB600B/tb600b.py
+++ Drivers/Micropython/TB600B/tb600b.py
@@ -14,11 +14,15 @@ class TB600B_CO:
         self.qna = True
     
     def send_command(self,cmd):
-        self.uart.write(cmd)
-        utime.sleep(1)
-        return self.uart.read()
+        """
+        Sends a command in QnA mode and returns the reply received from UART
+        """
+        self.uart.write(cmd) #Writes the desired command
+        utime.sleep(1) #Do not remove, this is a processing time
+        return self.uart.read() #Returns the reply sent from the sensor
     
     def changeMode(self):
+        """Changes between active upload and QnA mode. Active upload gives a lot of faulty data. Better to use passive."""
         if self.qna==True:
             self.send_command(ACTIVE_MODE)
             self.qna = False
@@ -27,28 +31,32 @@ class TB600B_CO:
             self.qna = True
     
     def led_off(self):
+        """Turns sensor LED OFF"""
         self.send_command(LIGHT_OFF)
     
     def led_on(self):
+        """Turns sensor LED ON"""
         self.send_command(LIGHT_ON)
     
     def read_gas(self):
+        """Reads only the Gas value"""
         if self.qna==True:
             gas_array = self.send_command(READ_GAS)
         else:
             utime.sleep(1)
             gas_array = self.uart.read()
         gas_array = list(gas_array)
-        return gas_array[2]*256 + gas_array[3]
+        return gas_array[2]*256 + gas_array[3] #Conversion into actual values, given in datasheet
     
     def read_all(self):
+        """Reads only the Gas values in ug/m3, ppb, temperature and humidity"""
         if self.qna==False:
             raise(Exception("You should be in QNA mode to do this. Call changeMode()?"))
         all_array = self.send_command(READ_ALL)
         all_array = list(all_array)
         readings = {}
-        readings["gas_ugm3"] = all_array[2]*256 + all_array[3]
-        readings["gas_ppb"] = all_array[6]*256 + all_array[7]
-        readings["temperature"] = ((all_array[8] <<8) | all_array[9])/100
-        readings["humidity"] = ((all_array[10]<<8) | all_array[11])/100
+        readings["gas_ugm3"] = all_array[2]*256 + all_array[3] #Conversion into actual values, given in datasheet
+        readings["gas_ppb"] = all_array[6]*256 + all_array[7] #Conversion into actual values, given in datasheet
+        readings["temperature"] = ((all_array[8] <<8) | all_array[9])/100 #Conversion into actual values, given in datasheet
+        readings["humidity"] = ((all_array[10]<<8) | all_array[11])/100 #Conversion into actual values, given in datasheet
         return readings
